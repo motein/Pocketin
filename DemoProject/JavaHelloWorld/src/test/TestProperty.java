2a8cbc663d7f065362b8a0dc46e404d388e5a485
Xiong An <xiong.an@keysight.com>
2017-10-27 18:14:49 +0800

    Add JavaHelloWorld

---
+++ DemoProject/JavaHelloWorld/src/test/TestProperty.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import hello.SayHello;
+
+public class TestProperty {
+
+	@Test
+	public void testHello() {
+		// fail("Not yet implemented");
+		new SayHello();
+		System.out.println("Hello");
+	}
+}

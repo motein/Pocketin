1244b471d2c8d32163961b5b317b9abe33273d84
Xiong An <xiong.an@keysight.com>
2017-11-28 16:45:42 +0800

    Add PM related materials.

---
+++ SoftwareDesign/Project Management/PM.mm
@@ -0,0 +1,17 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1511858211146" ID="ID_410272689" MODIFIED="1511858245819" TEXT="Project Management">
+<node CREATED="1511858252257" ID="ID_982984653" MODIFIED="1511858257948" POSITION="right" TEXT="Introduction"/>
+<node CREATED="1511858258642" ID="ID_303843780" MODIFIED="1511858281700" POSITION="right" TEXT="Project Life Cycle and Organization"/>
+<node CREATED="1511858292104" ID="ID_1050047984" MODIFIED="1511858309955" POSITION="right" TEXT="Project Management Processes for A Project"/>
+<node CREATED="1511858310504" ID="ID_1837787598" MODIFIED="1511858370986" POSITION="right" TEXT="Project Integration Management"/>
+<node CREATED="1511858371440" ID="ID_1084140225" MODIFIED="1511858387882" POSITION="right" TEXT="Project Scope Management"/>
+<node CREATED="1511858388246" ID="ID_768324112" MODIFIED="1511858409306" POSITION="right" TEXT="Project Time Management"/>
+<node CREATED="1511858409735" ID="ID_1790093265" MODIFIED="1511858425833" POSITION="right" TEXT="Project Cost Management"/>
+<node CREATED="1511858426205" ID="ID_1289581919" MODIFIED="1511858440617" POSITION="right" TEXT="Project Quality Management"/>
+<node CREATED="1511858441102" ID="ID_376865977" MODIFIED="1511858458585" POSITION="right" TEXT="Project Human Resource Management"/>
+<node CREATED="1511858469548" ID="ID_516039642" MODIFIED="1511858484128" POSITION="right" TEXT="Project Communications Management"/>
+<node CREATED="1511858484494" ID="ID_1647697536" MODIFIED="1511858505604" POSITION="right" TEXT="Project Risk Management"/>
+<node CREATED="1511858507798" ID="ID_1614695060" MODIFIED="1511858531160" POSITION="right" TEXT="Project Procurement Management"/>
+</node>
+</map>
---
+++ SoftwareDesign/Project Risk Management/Project Risk Management.mm
@@ -0,0 +1,194 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1511839694211" ID="ID_1378642042" MODIFIED="1511839731761" TEXT="Project Risk Management">
+<node CREATED="1511839741660" FOLDED="true" ID="ID_252130467" MODIFIED="1511848886829" POSITION="right" TEXT="Unit 1: Foundation of Risk">
+<node CREATED="1511844314169" FOLDED="true" ID="ID_255531709" MODIFIED="1511844408648" TEXT="Objectives">
+<node CREATED="1511844320990" ID="ID_1986043053" MODIFIED="1511844336849" TEXT="Define risk and its basic characteristics"/>
+<node CREATED="1511844338277" ID="ID_129794255" MODIFIED="1511844367049" TEXT="Describe the benefits of managing risk effectively"/>
+<node CREATED="1511844367460" ID="ID_1540645933" MODIFIED="1511844386092" TEXT="Recognize the effects and implications of risk"/>
+</node>
+<node CREATED="1511844410316" ID="ID_557739664" MODIFIED="1511844415495" TEXT="What is risk?">
+<node CREATED="1511844416629" ID="ID_1115616325" MODIFIED="1511844458651" TEXT="An uncertain event or condition that, if it occurs, has a positive or a negative effect on a project&apos;s objective."/>
+</node>
+<node CREATED="1511844482209" FOLDED="true" ID="ID_54610919" MODIFIED="1511844585594" TEXT="Dual Nature of Risk">
+<node CREATED="1511844495340" FOLDED="true" ID="ID_65036529" MODIFIED="1511844583738" TEXT="Threat">
+<node CREATED="1511844531938" ID="ID_259796621" MODIFIED="1511844550205" TEXT="Probability of occurrence"/>
+<node CREATED="1511844550713" ID="ID_1990930874" MODIFIED="1511844558597" TEXT="Consequence of failure"/>
+</node>
+<node CREATED="1511844500779" FOLDED="true" ID="ID_1890915955" MODIFIED="1511844584689" TEXT="Opportunity">
+<node CREATED="1511844562002" ID="ID_1044086303" MODIFIED="1511844569357" TEXT="Probability of success"/>
+<node CREATED="1511844569697" ID="ID_678194435" MODIFIED="1511844576516" TEXT="Benefit of success"/>
+</node>
+</node>
+<node CREATED="1511844591008" ID="ID_687476564" MODIFIED="1511844595724" TEXT="Elements of Risk">
+<node CREATED="1511844599250" ID="ID_976368148" MODIFIED="1511844688507" TEXT="a definable event (also discrete)"/>
+<node CREATED="1511844609368" ID="ID_95286918" MODIFIED="1511844618691" TEXT="Probability of occurrence"/>
+<node CREATED="1511844619063" ID="ID_1079246477" MODIFIED="1511844635132" TEXT="Impact (consequence of occurrence)"/>
+</node>
+<node CREATED="1511844713150" ID="ID_1111195452" MODIFIED="1511845428065" TEXT="Levels of Risk">
+<node CREATED="1511844810676" ID="ID_1518446914" MODIFIED="1511844823208" TEXT="Low threat or opportunity"/>
+<node CREATED="1511844823614" ID="ID_1565797055" MODIFIED="1511844835689" TEXT="Moderate threat or opportunity"/>
+<node CREATED="1511844836036" FOLDED="true" ID="ID_1931832733" MODIFIED="1511846045796" TEXT="High threat or opportunity">
+<node COLOR="#006699" CREATED="1511845423711" ID="ID_1093457522" MODIFIED="1511846036848">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="Project Risk Management_3236945423559417203.jpeg" width="700" height="500" />
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1511846094623" FOLDED="true" ID="ID_625111214" MODIFIED="1511846548646" TEXT="Characteristics of Risk Events">
+<node CREATED="1511846134270" FOLDED="true" ID="ID_1521540800" MODIFIED="1511846364253" TEXT="Situational">
+<node CREATED="1511846153886" ID="ID_1893216028" MODIFIED="1511846164761" TEXT="They have no textbook answer"/>
+<node CREATED="1511846165157" ID="ID_148143758" MODIFIED="1511846184193" TEXT="Team must rely on sound use of tools and methods/techniques"/>
+</node>
+<node CREATED="1511846139124" FOLDED="true" ID="ID_1242682916" MODIFIED="1511846363597" TEXT="Time-based">
+<node CREATED="1511846256204" ID="ID_1279916357" MODIFIED="1511846280134" TEXT="Risk is a future phenomenon only"/>
+<node CREATED="1511846280796" ID="ID_349745860" MODIFIED="1511846292094" TEXT="Time affects risk perceptions"/>
+</node>
+<node CREATED="1511846143541" FOLDED="true" ID="ID_1426510232" MODIFIED="1511846362893" TEXT="Interdependent">
+<node CREATED="1511846297499" ID="ID_1622225536" MODIFIED="1511846308590" TEXT="One risk event can affect others"/>
+<node CREATED="1511846308995" ID="ID_487504911" MODIFIED="1511846317302" TEXT="One risk can cause others"/>
+<node CREATED="1511846317586" ID="ID_469834684" MODIFIED="1511846333710" TEXT="Quantity of risks may affect perceptions"/>
+</node>
+<node CREATED="1511846391866" ID="ID_1665937570" MODIFIED="1511846403925" TEXT="Magnitude dependent">
+<node CREATED="1511846405201" ID="ID_1474547065" MODIFIED="1511846421428" TEXT="The greater the payoff, the more acceptable the risk"/>
+<node CREATED="1511846421792" ID="ID_509802493" MODIFIED="1511846443068" TEXT="The higher the impact, the more significant the risk"/>
+</node>
+<node CREATED="1511846447831" ID="ID_1952391403" MODIFIED="1511846452835" TEXT="Value based">
+<node CREATED="1511846453736" ID="ID_742258101" MODIFIED="1511846497531" TEXT="Personal and cultural values affect company risk taking"/>
+<node CREATED="1511846489838" ID="ID_1112134254" MODIFIED="1511846524010" TEXT="Company values affect individual choices"/>
+<node CREATED="1511846524334" ID="ID_1053624311" MODIFIED="1511846534882" TEXT="Everyone sees risk differently"/>
+</node>
+</node>
+<node CREATED="1511846550973" ID="ID_710216835" MODIFIED="1511846559737" TEXT="Risk Management">
+<node CREATED="1511846585807" ID="ID_544182714" MODIFIED="1511846645128" TEXT="..includes the processes concerned with conducting risk management planning, identification, analysis, responses, and monitoring and control on a project"/>
+<node CREATED="1511846704647" ID="ID_1618634308" MODIFIED="1511846789413" TEXT="Risk Management is integrated with the Project Planning Process, or we can say, it is performed throughout the project planning process"/>
+<node CREATED="1511846950585" ID="ID_929762225" MODIFIED="1511846987162" TEXT="Risk Management is a full project life-cycle responsibility"/>
+</node>
+<node CREATED="1511847032802" ID="ID_1712305355" MODIFIED="1511847052882" TEXT="Responsibilities in Risk Management">
+<node CREATED="1511847054911" FOLDED="true" ID="ID_1069801261" MODIFIED="1511847198435" TEXT="Project manager">
+<node CREATED="1511847069702" ID="ID_1341793100" MODIFIED="1511847089858" TEXT="Initiate and lead the risk management process"/>
+<node CREATED="1511847090158" ID="ID_242683792" MODIFIED="1511847126913" TEXT="Provide direction to the project team on the risk management process and tools"/>
+</node>
+<node CREATED="1511847061078" FOLDED="true" ID="ID_915145913" MODIFIED="1511847197444" TEXT="Project team">
+<node CREATED="1511847129765" ID="ID_1363476197" MODIFIED="1511847148248" TEXT="Understand and follow the risk management process"/>
+<node CREATED="1511847148645" ID="ID_434043073" MODIFIED="1511847172624" TEXT="Execute risk management strategies"/>
+<node CREATED="1511847178180" ID="ID_677716906" MODIFIED="1511847192167" TEXT="Report status on the risk management process"/>
+</node>
+</node>
+<node CREATED="1511847200532" ID="ID_566438874" MODIFIED="1511847218087" TEXT="Benifits of Risk Management">
+<node CREATED="1511847220676" ID="ID_1790437433" MODIFIED="1511847234927" TEXT="Minimize management by crisis"/>
+<node CREATED="1511847235316" ID="ID_609234550" MODIFIED="1511847250399" TEXT="Encourage proactive management"/>
+<node CREATED="1511847250665" ID="ID_916867366" MODIFIED="1511847260622" TEXT="Minimize surprise and problems"/>
+<node CREATED="1511847260883" ID="ID_810404103" MODIFIED="1511847273022" TEXT="Gain competitive advantage"/>
+<node CREATED="1511847273426" ID="ID_953654508" MODIFIED="1511847297322" TEXT="Decrease overall probability of project variances"/>
+<node CREATED="1511847299474" ID="ID_289924339" MODIFIED="1511847314085" TEXT="Increase probability of project success"/>
+<node CREATED="1511847314330" ID="ID_1057292821" MODIFIED="1511847327918" TEXT="Increase profitability"/>
+<node CREATED="1511847328921" ID="ID_1968373666" MODIFIED="1511847345061" TEXT="Focus on building the right product the first time"/>
+<node CREATED="1511847345385" ID="ID_1388489134" MODIFIED="1511847375341" TEXT="Prevent problems from occurring, or if they occur, from escalating"/>
+</node>
+<node CREATED="1511847382696" FOLDED="true" ID="ID_775837168" MODIFIED="1511847534003" TEXT="Types of Risks">
+<node CREATED="1511847390176" FOLDED="true" ID="ID_1594999337" MODIFIED="1511847488974" TEXT="Business risks">
+<node CREATED="1511847434608" ID="ID_1084571906" MODIFIED="1511847463667" TEXT="Normal risks of doing business that carry the potential of both gains and loses"/>
+</node>
+<node CREATED="1511847420215" ID="ID_1648141440" MODIFIED="1511847515943" TEXT="Pure, or insurable, risks">
+<node CREATED="1511847467663" ID="ID_627063206" MODIFIED="1511847484866" TEXT="Risks that present potential for loss only"/>
+</node>
+</node>
+<node CREATED="1511847537085" ID="ID_1180786383" MODIFIED="1511847544145" TEXT="Uncertainty">
+<node CREATED="1511847545014" ID="ID_1817907905" MODIFIED="1511847549665" TEXT="Known risks"/>
+<node CREATED="1511847550141" ID="ID_109567294" MODIFIED="1511847559432" TEXT="Unknown risks"/>
+</node>
+<node CREATED="1511847572413" ID="ID_761409092" MODIFIED="1511847588128" TEXT="Factors Affecting Risk Perceptions">
+<node CREATED="1511847591541" ID="ID_420315059" MODIFIED="1511847594744" TEXT="Control">
+<node CREATED="1511847622588" ID="ID_369073785" MODIFIED="1511847648775" TEXT="One can be placed at risk by oneself, another, the government, or acts of nature"/>
+<node CREATED="1511847649188" ID="ID_704378814" MODIFIED="1511847669367" TEXT="Control is related to lack of time, resources, or information"/>
+</node>
+<node CREATED="1511847595212" ID="ID_1387560047" MODIFIED="1511847600561" TEXT="Information">
+<node CREATED="1511847674772" ID="ID_441387461" MODIFIED="1511847683774" TEXT="Inadequate"/>
+<node CREATED="1511847684115" ID="ID_1500000610" MODIFIED="1511847688574" TEXT="Unfamiliar"/>
+<node CREATED="1511847688915" ID="ID_315532466" MODIFIED="1511847692422" TEXT="Unreliable"/>
+<node CREATED="1511847692666" ID="ID_1351791621" MODIFIED="1511847699350" TEXT="Unpredictable"/>
+<node CREATED="1511847699723" ID="ID_832164825" MODIFIED="1511847706407" TEXT="Insufficient time"/>
+</node>
+<node CREATED="1511847601532" ID="ID_750548186" MODIFIED="1511847602760" TEXT="Time">
+<node CREATED="1511847717346" ID="ID_283953311" MODIFIED="1511847746733" TEXT="The further into the future, the greater the degree of uncertainty"/>
+<node CREATED="1511847749178" ID="ID_1715291226" MODIFIED="1511847767773" TEXT="Lack of time to identify risk and perform risk management"/>
+<node CREATED="1511847768289" ID="ID_235165876" MODIFIED="1511847786285" TEXT="Ability to change actions today to improve future outcome"/>
+</node>
+<node CREATED="1511847603108" ID="ID_308338752" MODIFIED="1511847613584" TEXT="Risk preferences">
+<node CREATED="1511847790560" ID="ID_1240937491" MODIFIED="1511847796436" TEXT="Risk avoider"/>
+<node CREATED="1511847796680" ID="ID_1851046344" MODIFIED="1511847805997" TEXT="Risk taker(gambler)"/>
+<node CREATED="1511847806424" ID="ID_1294194960" MODIFIED="1511847811284" TEXT="Average person"/>
+</node>
+</node>
+<node CREATED="1511847841512" ID="ID_1625969420" MODIFIED="1511847858499" TEXT="Critical Success Factors">
+<node CREATED="1511847861193" ID="ID_1288042358" MODIFIED="1511847875675" TEXT="A process rooted in group techniques"/>
+<node CREATED="1511847875967" ID="ID_1170007855" MODIFIED="1511847892971" TEXT="A set of analytical tools that supports these factors"/>
+<node CREATED="1511847893183" ID="ID_1545847618" MODIFIED="1511847917338" TEXT="Continuous and ongoing planning and preparation for risk management"/>
+<node CREATED="1511847917670" ID="ID_701338381" MODIFIED="1511847943914" TEXT="Communications with team members and key stakeholders"/>
+</node>
+</node>
+<node CREATED="1511839751199" FOLDED="true" ID="ID_1476813828" MODIFIED="1511849557371" POSITION="right" TEXT="Unit 2: Planning for Risk Management and Identifying Risks">
+<node CREATED="1511848107562" FOLDED="true" ID="ID_151055077" MODIFIED="1511848892437" TEXT="Risk Management and Project Management">
+<node CREATED="1511848122890" ID="ID_1524943033" MODIFIED="1511848223181" TEXT="Project Management focuses on Planning, organizing, directing, and controlling an organization&apos;s resources to provide a product or service on schedule, within budget, and to the customer&apos;s stated requirements"/>
+<node CREATED="1511848252823" ID="ID_1569909173" MODIFIED="1511848299555" TEXT="Risk Management focuses on Identifying, quantifying, responding to and controlling the risks in a project"/>
+<node CREATED="1511848326151" ID="ID_1399209847" MODIFIED="1511848347250" TEXT="Risk Management is part of Project Management"/>
+</node>
+<node CREATED="1511848249937" FOLDED="true" ID="ID_599894912" MODIFIED="1511848891038" TEXT="Risk Management Process">
+<node CREATED="1511848411879" ID="ID_187410677" MODIFIED="1511848841584" TEXT="Inputs">
+<node CREATED="1511848429069" ID="ID_62497435" MODIFIED="1511848433968" TEXT="Requirements"/>
+<node CREATED="1511848434517" ID="ID_1440038289" MODIFIED="1511848437361" TEXT="People"/>
+<node CREATED="1511848437581" ID="ID_1664477789" MODIFIED="1511848445712" TEXT="Development process"/>
+<node CREATED="1511848445940" ID="ID_226675414" MODIFIED="1511848457024" TEXT="Management"/>
+<node CREATED="1511848457341" ID="ID_1386149277" MODIFIED="1511848461505" TEXT="Schedule"/>
+<node CREATED="1511848462300" ID="ID_987796920" MODIFIED="1511848471767" TEXT="Budget"/>
+<node CREATED="1511848464540" ID="ID_1138664180" MODIFIED="1511848479071" TEXT="Qualify"/>
+<node CREATED="1511848479420" ID="ID_383255224" MODIFIED="1511848486047" TEXT="Assumptions"/>
+<node CREATED="1511848487787" ID="ID_932297543" MODIFIED="1511848493135" TEXT="Constrains"/>
+<node CREATED="1511848493380" ID="ID_389633030" MODIFIED="1511848498672" TEXT="Testing"/>
+<node CREATED="1511848498891" ID="ID_720290126" MODIFIED="1511848505703" TEXT="Expectations"/>
+<node CREATED="1511848505988" ID="ID_1557167748" MODIFIED="1511848511879" TEXT="Deployment"/>
+<node CREATED="1511848512235" ID="ID_500858276" MODIFIED="1511848516287" TEXT="Security"/>
+<node CREATED="1511848516564" ID="ID_897076772" MODIFIED="1511848521327" TEXT="Training"/>
+<node CREATED="1511848521643" ID="ID_684460594" MODIFIED="1511848537750" TEXT="Risk Attitudes and Tolerances"/>
+</node>
+<node CREATED="1511848415837" FOLDED="true" ID="ID_1216040268" MODIFIED="1511848608115" TEXT="Tools and Techniques">
+<node CREATED="1511848544434" ID="ID_1787184643" MODIFIED="1511848550590" TEXT="Risk Management"/>
+</node>
+<node CREATED="1511848423509" ID="ID_794511340" MODIFIED="1511848842863" TEXT="Outputs">
+<node CREATED="1511848554522" ID="ID_620072416" MODIFIED="1511848563007" TEXT="Risk management plan"/>
+<node CREATED="1511848563250" ID="ID_1784695813" MODIFIED="1511848574631" TEXT="Risk register"/>
+<node CREATED="1511848575065" ID="ID_1747731626" MODIFIED="1511848583270" TEXT="Prioritized risks"/>
+<node CREATED="1511848583482" ID="ID_1360694429" MODIFIED="1511848591357" TEXT="Risk response plans"/>
+<node CREATED="1511848591570" ID="ID_292724811" MODIFIED="1511848600389" TEXT="Project plan updates"/>
+</node>
+</node>
+<node CREATED="1511848619498" FOLDED="true" ID="ID_404046012" MODIFIED="1511849556323" TEXT="ESI&apos;s Risk Management Model">
+<node CREATED="1511848657248" ID="ID_514083534" MODIFIED="1511848675956" TEXT="Step 1: Risk Management Planning">
+<node CREATED="1511848696617" ID="ID_84600444" MODIFIED="1511848712404" TEXT="Preliminary planning for addressing risk"/>
+<node CREATED="1511848712687" ID="ID_528356795" MODIFIED="1511848738402" TEXT="Initial development of a framework to be carried out during the project"/>
+</node>
+<node CREATED="1511848676328" FOLDED="true" ID="ID_995395272" MODIFIED="1511849555387" TEXT="Step 2: Identify Risks">
+<node CREATED="1511849007211" ID="ID_933456274" MODIFIED="1511849047181" TEXT="A structured and consistent approach to the identification of potential risk events"/>
+<node CREATED="1511849047641" ID="ID_242600732" MODIFIED="1511849074525" TEXT="A clear and specific description of the risk event"/>
+<node CREATED="1511849074945" ID="ID_368465571" MODIFIED="1511849087452" TEXT="A comprehensive list of risk events"/>
+<node CREATED="1511849089080" ID="ID_557666222" MODIFIED="1511849096116" TEXT="A team-based activity"/>
+</node>
+</node>
+</node>
+<node CREATED="1511839774806" ID="ID_187756061" MODIFIED="1511839795482" POSITION="right" TEXT="Unit 3: Analysis Fundamentals"/>
+<node CREATED="1511839795822" ID="ID_703128103" MODIFIED="1511839834377" POSITION="right" TEXT="Unit 4: Analyzing and Prioritizing Risk"/>
+<node CREATED="1511839837766" ID="ID_910853664" MODIFIED="1511839855528" POSITION="right" TEXT="Unit 5: Planning Response to Risk"/>
+<node CREATED="1511839855988" ID="ID_92504628" MODIFIED="1511849546825" POSITION="right" TEXT="Unit 6: Risk Execution, Evaluation, and Updating"/>
+<node CREATED="1511839875725" ID="ID_721554017" MODIFIED="1511839893743" POSITION="right" TEXT="Unit 7: Course Closeout"/>
+</node>
+</map>

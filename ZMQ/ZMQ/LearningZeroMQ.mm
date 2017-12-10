28421d3caf8d7b085dbebcc152fad91b907e4d0b
motein <motein@outlook.com>
2017-12-11 00:37:37 +0800

    添加0MQ

---
+++ ZMQ/ZMQ/LearningZeroMQ.mm
@@ -0,0 +1,23 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1512921907608" ID="ID_1729681743" MODIFIED="1512921998800">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4" color="#ff0000">Learning ZeroMQ</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1512922087625" ID="ID_1156584433" MODIFIED="1512922092682" POSITION="right" TEXT="Preface">
+<node CREATED="1512922110780" ID="ID_471386001" MODIFIED="1512922151419" TEXT="0MQ looks like an embeddable networking library but acts like a concurrency framework."/>
+<node CREATED="1512922163547" ID="ID_1677517595" MODIFIED="1512922231308" TEXT="It gives you sockets that carry atomic messages across various transports like in-process, inter-process, TCP, and multicast."/>
+<node CREATED="1512922255835" ID="ID_752796737" MODIFIED="1512922317760" TEXT="Can connect sockets N-to-N with patterns like fan-out, pub-sub, task distribution, and request-reply."/>
+</node>
+<node CREATED="1512922093229" ID="ID_131248346" MODIFIED="1512922093229" POSITION="right" TEXT=""/>
+</node>
+</map>

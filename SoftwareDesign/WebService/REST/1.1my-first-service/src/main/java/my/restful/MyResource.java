922c97256b9a94197b134db9fe7d65c4bb1c5f04
motein <motein@outlook.com>
2017-12-21 02:10:04 +0800

    添加ws文件

---
+++ SoftwareDesign/WebService/REST/1.1my-first-service/src/main/java/my/restful/MyResource.java
@@ -0,0 +1,31 @@
+package my.restful;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+	@Path("gotit")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+	
+	@GET
+	@Path("sayit")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String SayIt() {
+        return "Say it!";
+    }
+}

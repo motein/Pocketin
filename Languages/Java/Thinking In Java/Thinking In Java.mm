ed2c069f3fc778bd44eddd870c24e0b22defd018
Xiong An <xiong.an@keysight.com>
2018-01-09 11:08:08 +0800

    更新文档

--- Languages/Java/Thinking In Java/Thinking In Java.mm
+++ Languages/Java/Thinking In Java/Thinking In Java.mm
@@ -32,7 +32,7 @@
 <node CREATED="1512617027092" ID="ID_577259103" MODIFIED="1512617099346" TEXT="Access control(or implementation hiding) is about &quot;not  getting it right the first time&quot;. And that is why refactoring.">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1512617243208" ID="ID_1941069952" MODIFIED="1512617248727" TEXT="Package">
+<node CREATED="1512617243208" ID="ID_1941069952" MODIFIED="1514872182342" TEXT="Package">
 <node CREATED="1512617285439" ID="ID_117087346" MODIFIED="1512617290645" TEXT="The lib unit"/>
 <node CREATED="1512617249408" ID="ID_31950822" MODIFIED="1512617279298" TEXT="A package contains a group of classes, organized together under a single namespace."/>
 </node>
@@ -76,8 +76,54 @@
 <node CREATED="1512616544203" ID="ID_1859493146" MODIFIED="1512616615221" POSITION="right" TEXT="19. Enumerated Types">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1512616564161" ID="ID_1954453877" MODIFIED="1512616614476" POSITION="right" TEXT="20. Annotaions">
+<node CREATED="1512616564161" ID="ID_1954453877" MODIFIED="1514876448758" POSITION="right" TEXT="20. Annotaions">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1514873422536" ID="ID_1947348838" MODIFIED="1514873708892">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Annotations (also known as <font color="#ff0000">metadata</font>) provide a formalized way to add info to your code so that you can easily use that data at some later point.
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1514876450174" ID="ID_1793701639" MODIFIED="1514876622490">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      @Target defines where you can apply this annotation<font color="#ff0000">(a method or a field)</font>.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1514876497796" ID="ID_834293363" MODIFIED="1514876687298">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      @Retention defines whether the annotations are available in the source code(<b><font color="#ff0000">SOURCE</font></b>), in the class files(<b><font color="#ff0000">CLASS</font></b>), or at the run time(<b><font color="#ff0000">RUNTIME</font></b>).
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1514948166364" ID="ID_573579167" MODIFIED="1514948182758" TEXT="Annotation Elements">
+<node CREATED="1514948185090" ID="ID_815429993" MODIFIED="1514948193980" TEXT="All primitives"/>
+<node CREATED="1514948194233" ID="ID_81620018" MODIFIED="1514948198684" TEXT="String"/>
+<node CREATED="1514948958587" ID="ID_957078140" MODIFIED="1514948967175" TEXT="Class"/>
+<node CREATED="1514948967874" ID="ID_279031968" MODIFIED="1514948970126" TEXT="enums"/>
+<node CREATED="1514948970381" ID="ID_1937144534" MODIFIED="1514948974487" TEXT="Annotations"/>
+<node CREATED="1514948976042" ID="ID_868677336" MODIFIED="1514948986134" TEXT="Arrays of any of the above"/>
+</node>
+<node CREATED="1514965093516" ID="ID_1790374435" MODIFIED="1514965106468" TEXT="Annotations don&apos;t support inheritance"/>
 </node>
 <node CREATED="1512616577482" ID="ID_612605284" MODIFIED="1512616613205" POSITION="right" TEXT="21. Concurrency">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
